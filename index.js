1c035c545b7b701d9539590781b287905963eff3
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-11 16:46:34 -0400

    commit tree e0cd6684b932422a9d43fe9b2c7da09129241be9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function superbowlWin(array) {
+    const foundRecord = (array.find(array => array.result ==="W"));
+    if (foundRecord){
+        return foundRecord.year;
+    }
+    
+    // function findWin(obj){
+    //      return obj.result === "W";
+    // }
+    // let win = array.find(findWin);
+    // if (win){
+    //      return win.year;  
+    // }
 
+    // return array.find(findWin).year;
+    
+    // return array.find((obj) => obj.result === "W").year;
+}
